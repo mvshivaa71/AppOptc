772329e04d4561dbba15f345b32df15b30820383
Helenha BlogOPTC <mvshivaa71@gmail.com>
2016-11-25 20:11:18 +0100

    Traducir habilidades

--- common/data/abilities.js
+++ common/data/abilities.js
@@ -1,94 +1,94 @@
 window.abilities = [
 
     {
-        name: 'Damage reduction',
+        name: 'Reducción de Daño',
         levels: [
-            [ 5, 'Received damage reduced by 3%' ],
-            [ 10, 'Received damage reduced by 5%' ],
-            [ 15, 'Received damage reduced by 10%' ]
+            [ 5, 'Reduce el Daño en un 3%' ],
+            [ 10, 'Reduce el Daño en un 5%' ],
+            [ 15, 'Reduce el Daño en un 10%' ]
         ]
     },
 
     {
-        name: 'Charge specials',
+        name: 'Reducción de Cooldown',
         levels: [
-            [ 10, 'Cooldown of all specials reduced by 1 turn at the start of the fight' ],
-            [ 20, 'Cooldown of all specials reduced by 2 turns at the start of the fight' ]
+            [ 10, 'Reduce en 1 el Cooldown de las Especiales al inicio del Stage' ],
+            [ 20, 'Reduce en 2 el Cooldown de las Especiales al inicio del Stage' ]
         ]
     },
 
     {
-        name: 'Bind resistance',
+        name: 'Anti Encadenamientos',
         levels: [
-            [ 10, 'Bind duration reduced by 1 turn' ],
-            [ 15, 'Bind duration reduced by 2 turns' ],
-            [ 20, 'Bind duration reduced by 3 turns' ]
+            [ 10, 'Reduce en 1 la duración de Encadenamientos' ],
+            [ 15, 'Reduce en 2 la duración de Encadenamientos' ],
+            [ 20, 'Reduce en 3 la duración de Encadenamientos' ]
         ]
     },
 
     {
-        name: 'Silence resistance',
+        name: 'Anti Silencios',
         levels: [
-            [ 10, 'Silence duration reduced by 1 turn' ],
-            [ 15, 'Silence duration reduced by 2 turns' ],
-            [ 20, 'Silence duration reduced by 3 turns' ]
+            [ 10, 'Reduce en 1 la duración de Silencios' ],
+            [ 15, 'Reduce en 2 la duración de Silencios' ],
+            [ 20, 'Reduce en 3 la duración de Silencios' ]
         ]
     },
 
     {
-        name: 'Auto-heal',
+        name: 'Curación por turnos',
         levels: [
-            [ 5, '100 HP recovered at the end of each turn' ],
-            [ 8, '200 HP recovered at the end of each turn' ],
-            [ 12, '300 HP recovered at the end of each turn' ],
-            [ 16, '500 HP recovered at the end of each turn' ],
-            [ 24, '1000 HP recovered at the end of each turn' ]
+            [ 5, 'Cura 100 de HP cada turno' ],
+            [ 8, 'Cura 200 de HP cada turno' ],
+            [ 12, 'Cura 300 de HP cada turno' ],
+            [ 16, 'Cura 500 de HP cada turno' ],
+            [ 24, 'Cura 1000 de HP cada turno' ]
         ]
     },
 
     {
-        name: 'RCV boost',
+        name: 'Mejora de RCV',
         levels: [
-            [ 5, 'Crew\'s RCV boosted by 100 units' ],
-            [ 8, 'Crew\'s RCV boosted by 200 units' ],
-            [ 12, 'Crew\'s RCV boosted by 300 units' ],
-            [ 16, 'Crew\'s RCV boosted by 500 units' ],
-            [ 24, 'Crew\'s RCV boosted by 1000 units' ]
+            [ 5, 'Cura 100 de HP extra al coger una Carne de RCV' ],
+            [ 8, 'Cura 200 de HP extra al coger una Carne de RCV' ],
+            [ 12, 'Cura 300 de HP extra al coger una Carne de RCV' ],
+            [ 16, 'Cura 500 de HP extra al coger una Carne de RCV' ],
+            [ 24, 'Cura 1000 de HP extra al coger una Carne de RCV' ]
         ]
     },
 
     {
-        name: 'Slot rate boost',
+        name: 'Sincronización de Orbes',
         levels: [
-            [ 5, 'Boosted chances (20%) of getting matching orbs' ],
-            [ 10, 'Boosted chances (22.2%) of getting matching orbs' ],
-            [ 20, 'Boosted chances (25%) of getting matching orbs' ],
+            [ 5, 'Aumenta la aparición de Orbes Sincronizados en un 20%' ],
+            [ 10, 'Aumenta la aparición de Orbes Sincronizados en un 22.2%' ],
+            [ 20, 'Aumenta la aparición de Orbes Sincronizados en un 25%' ],
         ]
     },
 
     {
-        name: 'Poison resistance',
+        name: 'Resistencia al Veneno',
         levels: [
-            [ 10, 'Poison damage reduced by 20%' ],
-            [ 15, 'Poison damage reduced by 50%' ],
-            [ 20, 'Poison damage reduced by 100%' ],
+            [ 10, 'Reduce el Daño por Veneno un 20%' ],
+            [ 15, 'Reduce el Daño por Veneno un 50%' ],
+            [ 20, 'Reduce el Daño por Veneno un 100%' ],
         ]
     },
 
     {
-        name: 'Map damage resistance',
+        name: 'Reducción de Daño Ambiental',
         levels: [
-            [ 10, 'Map damage reduced by 50%' ],
-            [ 20, 'Map damage reduced by 100%' ],
+            [ 10, 'Reduce el Daño Ambiental un 50%' ],
+            [ 20, 'Reduce el Daño Ambiental un 100%' ],
         ]
     },
 
     {
-        name: 'Resilience',
+        name: 'Supervivencia',
         levels: [
-            [ 5, '5% chance to withstand a lethal enemy hit' ],
-            [ 10, '10% chance to withstand a lethal enemy hit' ],
-            [ 20, '20% chance to withstand a lethal enemy hit' ]
+            [ 5, 'Ínfima probabilidad de sobrevivir a un ataque con 1 de HP (5%)' ],
+            [ 10, 'Pequeña probabilidad de sobrevivir a un ataque con 1 de HP (10%)' ],
+            [ 20, 'Ocasional probabilidad de sobrevivir a un ataque con 1 de HP (20%)' ]
         ]
     }
 
