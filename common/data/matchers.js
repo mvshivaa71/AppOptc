b1fa92b7af41f849d344b7dcaedef62947607fa4
CelticsAFC <albertofc.es@gmail.com>
2016-12-23 20:56:22 +0100

    Update matchers.js

--- common/data/matchers.js
+++ common/data/matchers.js
@@ -93,7 +93,7 @@ window.matchers = [
     {
         name: 'Capitanes de orbes favorables',
         target: 'captain',
-        matcher: /aumentando.+ATK.+si tienen orbe favorable/i
+        matcher: /aumentando.+si tienen orbe favorable/i
     },
 
     {
