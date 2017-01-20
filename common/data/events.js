27bf70f56eed0dac8828e734af9c63edfba077a3
Helenha BlogOPTC <mvshivaa71@gmail.com>
2017-01-21 00:09:13 +0100

    Eventos Traducido

--- common/data/events.js
+++ common/data/events.js
@@ -2,151 +2,151 @@ window.events = {
     879: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     880: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     900: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     901: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     989: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     1036: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     1037: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     1243: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     1244: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     1291: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     1292: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     1305: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     1306: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Enabling [G] orbs support.' });
+                $scope.notify({ text: 'Activando orbes [G].' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     1313: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Captain ability still being worked on. Use a [G] orb for now to simulate a STR orb on a character. (Will only have 1.5x boost)' });
+                $scope.notify({ text: 'Se está implementando la habilidad de capitán de esta unidad. Utilice un orbe [G] para simular un orbe STR de una unidad (Sólo aumentará el ATK un 1.5x).' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
     1314: {
         onInsertion: function($scope) {
             if (++$scope.options.gOrbsEnabled == 1)
-                $scope.notify({ text: 'Captain ability still being worked on. Use a [G] orb for now to simulate a STR orb on a character. (Will only have 1.5x boost)' });
+                $scope.notify({ text: 'Se está implementando la habilidad de capitán de esta unidad. Utilice un orbe [G] para simular un orbe STR de una unidad (Sólo aumentará el ATK un 1.5x).' });
         },
         onRemoval: function($scope) {
             if (--$scope.options.gOrbsEnabled === 0)
-                $scope.notify({ text: 'Disabling [G] orbs support.' });
+                $scope.notify({ text: 'Desactivando orbes [G].' });
         }
     },
 };
