97a4864b45a0cb5ff646bd50e71ae477a0b10f78
Helenha BlogOPTC <mvshivaa71@gmail.com>
2017-01-29 17:02:33 +0100

    prueba calcu

--- damage/js/directives.js
+++ damage/js/directives.js
@@ -748,7 +748,8 @@ directives.special = function($rootScope) {
             scope.$watch('data.team[slot].unit',function(unit) {
                 removeType();
                 if (scope.tdata.team[scope.slot].special) element.addClass(unit.type);
-                scope.hasSpecial = unit && window.specials.hasOwnProperty(unit.number+1);
+				if(window.special !== undefined)
+					scope.hasSpecial = unit && window.specials.hasOwnProperty(unit.number+1);
             });
             element.click(function(e) {
                 isSelected = !isSelected;
